eef09ec66774141fbc5c1f3a3a629f97d348bdae
ARShenoy08 <f20260360@pilani.bits-pilani.ac.in>
2026-08-29 17:11:17 +0530

    Added modified q3

--- q3.py
+++ q3.py
@@ -2,7 +2,7 @@ def is_prime(n):
     if n < 2:
         return False
     k=0
-    for i in range(1,int(n**0.5)):
+    for i in range(1,int(n**0.5)+1):
         if n%i==0:
             k+=1
         if k>1:
